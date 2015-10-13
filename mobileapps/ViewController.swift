72a9d34a7f51de8d060ab7266292e2980c52b4bd
Matthew Plummer <Matt@Matthews-MacBook-Pro-2.local>
2015-10-13 15:15:50 -0400

    Initial Commit

---
+++ mobileapps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobileapps
+//
+//  Created by Matthew Plummer on 10/13/15.
+//  Copyright (c) 2015 Matthew Plummer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
